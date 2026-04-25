c9f5a6558c6278237180b3b71f9fd0ecc0f1c9f3
harhith1188 <itzmyworkemail1188@gmail.com>
2026-04-25 00:33:56 -0700

    updated appliancesScreen

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -1,35 +1,36 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function Tabnav(){
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <MaterialIcons name="home" size={28}/>,
         }}
       />
+
+
+      <Tabs.Screen
+        name="applianceScreen"
+        options={{
+          title: 'appliances',tabBarIcon: ({ color }) => <MaterialIcons name="devices" size={28}/>,}}
+      />
+
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
+          title: 'savings',tabBarIcon: ({ color }) => <MaterialIcons name="savings" size={28}/>,}}
       />
+
+
+
     </Tabs>
+
+    
   );
 }
---
+++ app/(drawer)/(tabs)/applianceScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default  function Appliances(){
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>hello this  is appliances </Text>
+            </View>
+        </SafeAreaView>
+    )
+}
