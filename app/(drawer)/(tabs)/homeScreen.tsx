5c42516261a9a8ade3688bd0783df3cf12a315da
harhith1188 <itzmyworkemail1188@gmail.com>
2026-04-25 10:40:26 -0700

    my works

--- app/(drawer)/(tabs)/homeScreen.tsx
+++ app/(drawer)/(tabs)/homeScreen.tsx
@@ -1,12 +1,75 @@
-import { Text, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default  function HomeScreen(){
   return(
-  <SafeAreaView>
-    <View>
-      <Text>hello this is home screen
-      </Text>
+  <SafeAreaView style={{flex:1,backgroundColor:'white'}}>
+  <ScrollView contentContainerStyle={{alignItems:'center',justifyContent:'center',gap:30}} style={{flex:1}}>
+    
+    {/* c1     */}
+    <View  style={styles.c1}>
+    <Text style={styles.h2}>Hello user 🖐️</Text>
+    <Text style={styles.h3}>Here's your electricity overview</Text>
+    </View>
+
+    {/* c2 */}
+    <View style={styles.c2}>
+      
+      <View style={styles.c2_1}> 
+      <MaterialIcons name="wallet" color={"white"} size={39} />
       </View>
+
+      <View style={styles.c2_2}>
+        <Text style={styles.h2}>Estimated Bill</Text>
+      </View>
+
+
+    </View>
+    
+    </ScrollView>
+  
   </SafeAreaView>
 )
 }
+
+const styles= StyleSheet.create({
+  c1:{
+    minHeight:100,
+    width:"90%",
+    padding:20,
+    justifyContent:"space-evenly",
+    alignItems:'center',
+    borderWidth:0.5,
+    borderColor:'rgba(0,0,0,0.3)',
+    borderRadius:12,
+    backgroundColor:"rgba(206, 196, 196, 0.3)"
+  },
+  h2:{
+    fontSize:22,
+    textTransform:'capitalize',
+    fontWeight:'bold'
+  },
+  h3:{
+    fontSize:16
+  },
+  c2:{
+    minHeight:200,
+    borderRadius:10,
+    width:"90%",
+    borderWidth:0.5,
+    borderColor:'rgba(0,0,0,0.3)',
+    alignItems:'center',
+    flexDirection:'row',
+    justifyContent:'space-evenly',
+    backgroundColor:"rgba(206, 196, 196, 0.3)"
+  },
+  c2_1:{
+    height:80,
+    width:80,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'green',
+    borderRadius:100
+  }
+
+})
--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,10 +1,29 @@
 import { Drawer } from "expo-router/drawer";
+import { Image, Text, View } from "react-native";
 export  default function Drawernavigate(){
     return(
         <Drawer screenOptions={{headerShown:false}}>
-            <Drawer.Screen name="(tabs)" options={{headerShown:true ,title:'home'}}/> 
-            <Drawer.Screen name="profileScreen" options={{headerShown:true}}/>
-            <Drawer.Screen name="settingsScreen" options={{headerShown:true}}/>
+          
+          
+          <Drawer.Screen
+              name="(tabs)"options={{headerShown: true,title: "home",headerStyle:{height:150},
+                headerTitle: () => (
+                <View style={{ flexDirection: "row", alignItems: "center",justifyContent:'space-evenly' }}>
+                    <Image
+                        source={require("../../assets/images/app_logo.png")} // 👈 your logo path
+                        style={{ width:80, height:80, marginRight: 8 }}
+                        resizeMode="contain"
+                    />
+                    <Text style={{ fontSize: 20, fontWeight: "bold" }}>Smart Electricity</Text>
+                </View>
+            ),
+  
+  
+        }}
+        />
+            
+        <Drawer.Screen name="profileScreen" options={{headerShown:true}}/>
+        <Drawer.Screen name="settingsScreen" options={{headerShown:true}}/>
          </Drawer>   
     )
 }
