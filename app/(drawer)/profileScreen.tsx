4fb9cfeab025f5f2bf66d8f8731e4ca2da827dda
harhith1188 <itzmyworkemail1188@gmail.com>
2026-04-24 21:29:06 -0700

    modified profile

--- app/(drawer)/profileScreen.tsx
+++ app/(drawer)/profileScreen.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Profile(){
@@ -7,6 +7,7 @@ export default function Profile(){
             <ScrollView style={{flex:1}}>
             <View>
                 <Text>hello this is profile screen</Text>
+                <Image source={require("../../assets/images/app_logo.png")} style={{height:100,width:100}}/>
             </View>
             </ScrollView>
         </SafeAreaView>
